e03f15d291bc0af2e2c57852acf18583826b8659
Sai Kumar Manakan <msk.mymails@gmail.com>
2017-03-30 13:27:46 -0500

    Added Test DB

---
+++ SQL/MySQL/tutorial.sql
@@ -0,0 +1,41 @@
+--###############################################
+
+-- MySQL
+
+-- Last updated : 
+
+--###############################################
+
+-- MySQL command line 
+-- C:> mysql --user=<username> --password=<password> <db_name>
+
+-- Import SQL
+-- C:> mysql -t < <sql_file>
+
+-- Export to SQL
+
+--###############################################
+
+-- View Databases
+
+SHOW DATABASES;
+
+-- View users
+
+SELECT * FROM mysql.user;
+
+-- Select a Database
+
+USE <db_name>;
+
+-- Show tables
+
+SHOW TABLES;
+
+-- Describe table
+
+DESC <table_name>;
+
+-- Select queries
+
+
---
+++ SQL/Oracle/tutorial.sql
@@ -0,0 +1,25 @@
+--###############################################
+
+-- Oracle
+
+-- Last updated : 
+
+--###############################################
+
+-- View Databases
+
+SHOW DATABASES;
+
+-- Select a Database
+
+USE <db_name>;
+
+-- Show tables
+
+SHOW TABLES;
+
+-- Describe table
+
+DESC <table_name>;
+
+-- Select queries
