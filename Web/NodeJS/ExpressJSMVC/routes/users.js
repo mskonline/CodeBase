e7bf0ede7629a0b470245f9589a633082aa5ecb9
Sai Kumar Manakan <saikumar.manakan@gmail.com>
2019-09-20 01:23:38 -0500

    Minor tweaks

--- Web/NodeJS/ExpressJSMVC/routes/users.js
+++ Web/NodeJS/ExpressJSMVC/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/post', function(req, res, next) {
+    var data = {name : "POST"};
+    res.send('respond with a resources ' + data.name);
+});
+
 module.exports = router;
